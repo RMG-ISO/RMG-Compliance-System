0229689a98ad4c76f5e86ea74d8722929bec79d5
Alaa wahbah <a.wahbah@rmg-sa.com>
2021-11-08 09:23:16 +0200

    Config files

--- Code/angular/src/environments/environment.prod.ts
+++ Code/angular/src/environments/environment.prod.ts
@@ -9,13 +9,19 @@ export const environment = {
     name: 'ComplianceSystem',
     logoUrl: 'assets/logo.png',
   },
+  // oAuthConfig: {
+  //   issuer: 'https://iso.digitaltransformationinstitute.org:11000',
+  //   redirectUri: baseUrl,
+  //   clientId: 'ComplianceSystem_App',
+  //   responseType: 'code',
+  //   scope: 'offline_access ComplianceSystem',
+  //   requireHttps: true
+  // },
   oAuthConfig: {
     issuer: 'https://iso.digitaltransformationinstitute.org:11000',
-    redirectUri: baseUrl,
     clientId: 'ComplianceSystem_App',
-    responseType: 'code',
-    scope: 'offline_access ComplianceSystem',
-    requireHttps: true
+    dummyClientSecret: '1q2w3e*',
+    scope: 'offline_access openid profile role email phone ComplianceSystem',
   },
   apis: {
     default: {
