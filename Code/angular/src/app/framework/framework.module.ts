a3ab6faec4a48440ce5e13dc900320cdfd956103
Alaa wahbah <a.wahbah@rmg-sa.com>
2021-11-11 09:39:16 +0200

    remove auto foucus and un used component

--- Code/angular/src/app/framework/framework.module.ts
+++ Code/angular/src/app/framework/framework.module.ts
@@ -5,22 +5,16 @@ import { CommonModule } from '@angular/common';
 
 import { FrameworkComponent } from './framework.component';
 import { FrameworkRoutingModule } from './framework-routing.module';
-import { AddFrameworkComponent } from './add-framework/add-framework.component';
 
 
 @NgModule({
   declarations: [
     FrameworkComponent,
-    AddFrameworkComponent
-  ],
-  entryComponents:[
-    AddFrameworkComponent
-  ],
+  ], 
   imports: [
     CommonModule,
     FrameworkRoutingModule,
     SharedModule,
-    MatCardModule
   ]
 })
 export class FrameworkModule { }
