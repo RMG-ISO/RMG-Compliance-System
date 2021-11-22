4cf84e3138a3ab81d2e9432e83b38b5b582a435e
Alaa wahbah <a.wahbah@rmg-sa.com>
2021-11-22 14:13:55 +0200

    asd

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.EntityFrameworkCore/Migrations/20211122121334_Update1AssessmentEntity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RMG.ComplianceSystem.Migrations
+{
+    public partial class Update1AssessmentEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AttaChmentId",
+                table: "AppAssessments",
+                newName: "AttachmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AttachmentId",
+                table: "AppAssessments",
+                newName: "AttaChmentId");
+        }
+    }
+}
