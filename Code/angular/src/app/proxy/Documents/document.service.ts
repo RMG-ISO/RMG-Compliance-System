2e0bd33721c7986b3643ebee512f17233b8c4e6a
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-07-30 17:50:24 +0200

    Create Proxy document

---
+++ Code/angular/src/app/proxy/Documents/document.service.ts
@@ -0,0 +1,96 @@
+import type { CreateUpdateDocumentDto, DocumentDto, DocPagedAndSortedResultRequestDto,DocumentCategoryDto,CreateUpdateDocumentCategoryDto,DocCategoryPagedAndSortedResultRequestDto } from './dtos/models';
+import { RestService } from '@abp/ng.core';
+import type { ListResultDto, PagedResultDto } from '@abp/ng.core';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class documentService {
+  apiName = 'Default';
+
+  create = (input: CreateUpdateDocumentDto) =>
+    this.restService.request<any, DocumentDto>({
+      method: 'POST',
+      url: '/api/app/document',
+      body: input,
+    },
+    { apiName: this.apiName });
+
+  delete = (id: string) =>
+    this.restService.request<any, void>({
+      method: 'DELETE',
+      url: `/api/app/document/${id}`,
+    },
+    { apiName: this.apiName });
+
+  get = (id: string) =>
+    this.restService.request<any, DocumentDto>({
+      method: 'GET',
+      url: `/api/app/document/${id}`,
+    },
+    { apiName: this.apiName });
+
+
+  getList = (input: DocPagedAndSortedResultRequestDto) =>
+    this.restService.request<any, PagedResultDto<DocumentDto>>({
+      method: 'GET',
+      url: '/api/app/document/document-by-category',
+      params: { search: input.search, categoryId: input.CategoryId, sorting: input.sorting, skipCount: input.skipCount, maxResultCount: input.maxResultCount },
+    },
+    { apiName: this.apiName });
+
+  update = (id: string, input: CreateUpdateDocumentCategoryDto) =>
+    this.restService.request<any, DocumentDto>({
+      method: 'PUT',
+      url: `/api/app/framework/${id}`,
+      body: input,
+    },
+    { apiName: this.apiName });
+   //////Document Category //////////////////
+    createCategory = (input: CreateUpdateDocumentCategoryDto) =>
+    this.restService.request<any, DocumentCategoryDto>({
+      method: 'POST',
+      url: '/api/app/document',
+      body: input,
+    },
+    { apiName: this.apiName });
+
+  deleteCategory = (id: string) =>
+    this.restService.request<any, void>({
+      method: 'DELETE',
+      url: `/api/app/document/${id}`,
+    },
+    { apiName: this.apiName });
+
+  getCategory = (id: string) =>
+    this.restService.request<any, DocumentCategoryDto>({
+      method: 'GET',
+      url: `/api/app/document/${id}`,
+    },
+    { apiName: this.apiName });
+
+
+  getListCategory = (input: DocCategoryPagedAndSortedResultRequestDto) =>
+    this.restService.request<any, PagedResultDto<DocumentCategoryDto>>({
+      method: 'GET',
+      url: '/api/app/document/document-by-category',
+      params: { search: input.search,sorting: input.sorting, skipCount: input.skipCount, maxResultCount: input.maxResultCount },
+    },
+    { apiName: this.apiName });
+
+  updateCategory = (id: string, input: CreateUpdateDocumentDto) =>
+    this.restService.request<any, DocumentCategoryDto>({
+      method: 'PUT',
+      url: `/api/app/framework/${id}`,
+      body: input,
+    },
+    { apiName: this.apiName });
+
+
+
+
+
+
+  constructor(private restService: RestService) {}
+}
