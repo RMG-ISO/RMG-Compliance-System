4d590d8eba6cb594fc771e02b80897728ba2bdac
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-08-09 16:50:45 +0200

    Create StaticDataAppService to static Data to Risks And opportunities

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application/StaticData/StaticDataAppService.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Users;
+using Microsoft.AspNetCore.Authorization;
+using RMG.ComplianceSystem.Attachments.Dtos;
+using RMG.ComplianceSystem.Risks;
+using RMG.ComplianceSystem.Risks.Dtos;
+using RMG.ComplianceSystem.Permissions;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Identity;
+using RMG.ComplianceSystem.Risks.Entity;
+using RMG.ComplianceSystem.Risks.IRepository;
+
+namespace RMG.ComplianceSystem.StaticData
+{
+    // [Authorize(ComplianceSystemPermissions.Risk.Default)]
+    public class StaticDataAppService :
+        CrudAppService<
+            StaticDatatb, //The Risk entity
+            StaticDataDto, //Used to show Risks
+            Guid, //Primary key of the Risk entity
+            StaticDataPagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateStaticDataDto>, //Used to create/update a Risk
+        IStaticDataAppService //implement the IRiskAppService
+    {
+        //   Start Permissions
+        #region Start Permissions
+        protected override string GetPolicyName { get; set; } = ComplianceSystemPermissions.StaticData.Default;
+        protected override string GetListPolicyName { get; set; } = ComplianceSystemPermissions.StaticData.Default;
+        protected override string CreatePolicyName { get; set; } = ComplianceSystemPermissions.StaticData.Create;
+        protected override string UpdatePolicyName { get; set; } = ComplianceSystemPermissions.StaticData.Update;
+        protected override string DeletePolicyName { get; set; } = ComplianceSystemPermissions.StaticData.Delete;
+        #endregion
+        // End Permissions
+        //Start Properties and Constructor RiskAppService
+        #region Start Properties and Constructor RiskAppService
+        private readonly IStaticDataRepository StaticDataRepository;
+        private readonly IdentityUserManager User;
+
+        public StaticDataAppService(IdentityUserManager _User,  IStaticDataRepository _StaticDatarepository) : base(_StaticDatarepository)
+        {
+            StaticDataRepository = _StaticDatarepository;
+            User = _User;
+        }
+        #endregion
+        //End Properties and Constructor RiskAppService
+        //Start Methods getbyId and GetListRiskByCategory
+        #region Start Methods getbyId and GetListRiskByCategory
+        public async Task<PagedResultDto<StaticDataDto>> GetListByFilterAsync(StaticDataPagedAndSortedResultRequestDto input)
+        {
+            List<StaticDataDto> Risks = new List<StaticDataDto>();
+            if (input.Type!=null)
+            {
+                //get Risk By CategoryId and Filters and Pagination
+                var ListRisks = StaticDataRepository.Where(x => x.Type == input.Type &&
+                ((x.NameAr.Contains(input.Search) || input.Search.IsNullOrEmpty()) || (x.NameEn.Contains(input.Search) || input.Search.IsNullOrEmpty())))
+                 .Skip(input.SkipCount).Take(input.MaxResultCount).ToList();
+                // Mapping Risk to RiskDto
+                Risks = ObjectMapper.Map<List<StaticDatatb>, List<StaticDataDto>>(ListRisks);
+            }
+            else
+            {
+                //get Risk By CategoryId and Filters and Pagination
+              var  ListDoc = StaticDataRepository.Where(x => 
+                (x.NameAr.Contains(input.Search) || input.Search.IsNullOrEmpty()) || (x.NameEn.Contains(input.Search) || input.Search.IsNullOrEmpty()))
+                 .Skip(input.SkipCount).Take(input.MaxResultCount).ToList();
+                // Mapping Risk to RiskDto
+                Risks = ObjectMapper.Map<List<StaticDatatb>, List<StaticDataDto>>(ListDoc);
+            }
+           
+            //Get the total count with Risk
+            var totalCount = Risks.Count;
+            // return RiskDtos and totalCount
+            return new PagedResultDto<StaticDataDto>(
+                totalCount,
+                Risks
+            );
+        }
+
+
+        #endregion
+
+
+    }
+}
