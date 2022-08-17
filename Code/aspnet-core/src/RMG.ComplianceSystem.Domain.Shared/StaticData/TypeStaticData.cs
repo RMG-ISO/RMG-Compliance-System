e40942de10b98fad5ef7657fc1eb6f69e409589f
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-08-17 22:10:09 +0200

    create getTypeStaticData and localization

--- Code/aspnet-core/src/RMG.ComplianceSystem.Domain.Shared/StaticData/TypeStaticData.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Domain.Shared/StaticData/TypeStaticData.cs
@@ -6,7 +6,13 @@ namespace RMG.ComplianceSystem.StaticData
 {
     public enum TypeStaticData
     {
-        Sector=1, Standard=2, GeneralDepartment=3, Department=4, Category=5
+        Sector=1, Department=4, Category=5
+    }
+    public class getEnumTypeStaticData
+    {
+        public int Id { get; set; }
+        public string NameEn { get; set; }
+        public string NameAr { get; set; }
     }
 
 }
