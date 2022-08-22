0643d6616566658c4d5c24083d4f8be785edc65a
m.mohammed <m.mohammed@rmg-sa.com>
2022-08-23 00:13:38 +0200

    fix things

--- Code/angular/src/app/risks-mangement/create/first/first.component.ts
+++ Code/angular/src/app/risks-mangement/create/first/first.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StaticDataService } from '@proxy/StaticData';
+import { Type } from '../../list/list.component';
 
 @Component({
   selector: 'app-first',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.scss']
 })
 export class FirstComponent implements OnInit {
+  @Input('form') form;
+  Type = Type;
+  
 
-  constructor() { }
+  constructor(
+    private staticDataService:StaticDataService,
 
+  ) { }
+
+  sectors;
+  departments;
+  categories
   ngOnInit(): void {
+    this.getList(1, 'sectors');
+    this.getList(2, 'departments');
+    this.getList(3, 'categories');
+  }
+
+  getList(Type, key) {
+    this.staticDataService.getList({Type:Type, search:null, maxResultCount:null }).subscribe(r => {
+      this[key] = r.items;
+    })
   }
 
 }
