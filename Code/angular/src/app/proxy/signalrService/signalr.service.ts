9e43ae6f256b67f3788280d42f0b56995bc7a8c9
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-09-08 13:22:30 +0200

    Create SignlR  Service In Proxy

--- Code/angular/src/app/proxy/signalrService/signalr.service.ts
+++ Code/angular/src/app/proxy/signalrService/signalr.service.ts
@@ -1,34 +1,65 @@
+
 import { Injectable } from '@angular/core';
-import * as signalR from "@microsoft/signalr";
+import * as signalR from '@microsoft/signalr';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class SignalrService {
-  apiName = 'Default';
-  public data:[];
-  private hubConnection: signalR.HubConnection
-    public startConnection = () => {
-      this.hubConnection = new signalR.HubConnectionBuilder()
-                              .withUrl(this.apiName +'Dashboard')
-                              .build();
-      this.hubConnection
-        .start()
-        .then(() => console.log('Connection started'))
-        .catch(err => console.log('Error while starting connection: ' + err))
-    }
+  connection: signalR.HubConnection;
+  private isNotificationHubConnected = false
 
-    public addTreatmentRisksListener = () => {
-      this.hubConnection.on('TreatmentRisks', (data) => {
-        debugger;
-        this.data = data;
-        console.log(data);
+  constructor() { }
+
+  public initiateSignalrConnection(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.connection = new signalR.HubConnectionBuilder()
+        .withUrl(`${environment.apis.default.url}/Dashboard`) // the SignalR server url
+        .build();
+
+        this.configureConnection(this.connection);
+
+      // this.connection
+      //   .start()
+      //   .then(() => {
+      //     resolve(true);
+      //   })
+      //   .catch((error) => {
+      //     reject();
+      //   });
+
+    });
+  }
+
+  configureConnection(connection): void {
+    // debugger
+    ////Reconnect loop
+    function start() {
+      return new Promise(function (resolve, reject) {
+        connection.start()
+          .then(resolve)
+          .catch(() => {
+            setTimeout(() => {
+              start().then(resolve);
+            }, 5000);
+          });
       });
     }
+    // Reconnect if hub disconnects
 
+    connection.onclose(e => {
+      this.isNotificationHubConnected = false;
+      start().then(() => {
+        this.isNotificationHubConnected = true;
+      });
+    });
 
-
-
-
+    start().then(() => {
+      this.isNotificationHubConnected = true;
+    });
+    // Register to get notifications
+  }
 }
+
