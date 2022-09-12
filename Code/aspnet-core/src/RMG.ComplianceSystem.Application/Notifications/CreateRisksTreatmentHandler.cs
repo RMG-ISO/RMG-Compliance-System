fb9e423e8cabb22e4a87b02b19e6900df09506a6
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-09-12 15:41:22 +0200

    remove unusing Libraries to CreateRisksTreatmentHandler

--- Code/aspnet-core/src/RMG.ComplianceSystem.Application/Notifications/CreateRisksTreatmentHandler.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application/Notifications/CreateRisksTreatmentHandler.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using RMG.ComplianceSystem.EmailTemplates;
-using RMG.ComplianceSystem.EmailTemplates.Dtos;
 using RMG.ComplianceSystem.Employees;
 using RMG.ComplianceSystem.Localization;
 using RMG.ComplianceSystem.Risks.IRepository;
