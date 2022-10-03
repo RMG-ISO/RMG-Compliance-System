2d4da59346c5b7c991c3bb8944cf7263b337887a
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-10-03 18:42:19 +0200

    update

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Frameworks/Dtos/ComplainceDto.cs
@@ -0,0 +1,31 @@
+﻿using RMG.ComplianceSystem.Assessments.Dtos;
+using RMG.ComplianceSystem.Controls.Dtos;
+using RMG.ComplianceSystem.Domains.Dtos;
+using System.Collections.Generic;
+
+namespace RMG.ComplianceSystem.Frameworks.Dtos
+{
+    public class ComplainceDto
+    {
+        public int Applicable { get; set; }
+        public int NotApplicable { get; set; }
+        public FrameworkData FrameworkData { get; set; }
+
+    }
+    public class FrameworkData
+    {
+        public FrameworkDto FrameworkDto { get; set; }
+        public List<DomainsDta> DomaindDta { get; set; }
+    }
+    public class DomainsDta
+    {
+       public DomainDto subdomain { get; set; }
+        public List<ControlsDto> ChildrenControls { get; set; }
+    }
+    public class ControlsDto
+    {
+        public ControlDto subControl { get; set; }
+        public AssessmentDto AssessmentDto { get; set; }
+    }
+
+}
--- Code/aspnet-core/src/RMG.ComplianceSystem.Application/Frameworks/FrameworkAppService.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application/Frameworks/FrameworkAppService.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
+using RMG.ComplianceSystem.Assessments;
+using RMG.ComplianceSystem.Domains;
+using RMG.ComplianceSystem.Controls;
+using RMG.ComplianceSystem.Domains.Dtos;
+using RMG.ComplianceSystem.Controls.Dtos;
+using RMG.ComplianceSystem.Assessments.Dtos;
 
 namespace RMG.ComplianceSystem.Frameworks
 {
@@ -19,11 +25,21 @@ namespace RMG.ComplianceSystem.Frameworks
         protected override string UpdatePolicyName { get; set; } = ComplianceSystemPermissions.Framework.Update;
         protected override string DeletePolicyName { get; set; } = ComplianceSystemPermissions.Framework.Delete;
 
+
+        private readonly IAssessmentRepository _assessmentRepository;
+        private readonly IDomainRepository _domainRepository;
+        private readonly IControlRepository _controlRepository;
         private readonly IFrameworkRepository _repository;
 
-        public FrameworkAppService(IFrameworkRepository repository) : base(repository)
+        public FrameworkAppService(IFrameworkRepository repository,
+            IDomainRepository domainRepository,
+        IControlRepository controlRepository,
+            IAssessmentRepository assessmentRepository) : base(repository)
         {
             _repository = repository;
+            _assessmentRepository = assessmentRepository;
+            _domainRepository = domainRepository;
+            _controlRepository = controlRepository;
         }
 
 
@@ -37,7 +53,7 @@ namespace RMG.ComplianceSystem.Frameworks
                 t.ShortcutEn.Contains(input.Search) ||
                 t.DescriptionAr.Contains(input.Search) ||
                 t.DescriptionEn.Contains(input.Search))
-                .WhereIf(input.Status.HasValue,t=>t.Status==input.Status);
+                .WhereIf(input.Status.HasValue, t => t.Status == input.Status);
         }
 
         protected override Task<Framework> GetEntityByIdAsync(Guid id)
@@ -45,11 +61,66 @@ namespace RMG.ComplianceSystem.Frameworks
             return Repository.GetAsync(id);
         }
 
-       [Authorize]
+        [Authorize]
         public async Task<ListResultDto<FrameworkDto>> GetFrameworkListLookupAsync()
         {
             var data = await Repository.GetListAsync();
             return new ListResultDto<FrameworkDto>(ObjectMapper.Map<List<Framework>, List<FrameworkDto>>(data));
+        }
+
+
+        public async Task<List<ComplainceDto>> GetFrameWorkData()
+        {
+            //var Applicable = _assessmentRepository.Where(t => t.Applicable == ApplicableType.Applicable).Count();
+            //var NotApplicable = _assessmentRepository.Where(t => t.Applicable == ApplicableType.NotApplicable).Count();
+            var listComplaince = new List<ComplainceDto>();
+
+            var framworks = _repository.ToList();
+            foreach (var item in framworks)
+            {
+
+                var Complaince = new ComplainceDto();
+                var DomainsDta = new DomainsDta();
+                var FrameworkDt=new FrameworkData();
+
+                FrameworkDt.FrameworkDto=ObjectMapper.Map<Framework, FrameworkDto>(item);
+
+                var domainsWithChild = _domainRepository.Where(c => c.FrameworkId == item.Id).ToList();
+                foreach (var domains in domainsWithChild)
+                {
+                    var ListDomains = new List<DomainsDta>();
+                    if (domains.Children != null)
+                        foreach (var domain in domains.Children)
+                        {
+                            var domainDto = ObjectMapper.Map<Domain, DomainDto>(domain);
+                           DomainsDta.subdomain= domainDto;
+                            var ControlsDto =new List<ControlsDto>();
+                            var controlsWithChild = _controlRepository.Where(e => e.DomainId == domain.Id).ToList();
+                            foreach (var control in controlsWithChild)
+                            {
+                                var Ctrol = new ControlsDto();
+                                if (control.Children != null)
+                                    foreach (var ctrl in control.Children)
+                                    {
+                                        Ctrol.subControl=ObjectMapper.Map<Control, ControlDto>(ctrl);
+                                        Ctrol.AssessmentDto = ObjectMapper.Map<Assessment, AssessmentDto>(_assessmentRepository.Where(u => u.ControlId == ctrl.Id).FirstOrDefault());
+                                       if(Ctrol.AssessmentDto.Applicable== ApplicableType.Applicable)
+                                        Complaince.Applicable +=1 ;
+                                        if (Ctrol.AssessmentDto.Applicable == ApplicableType.Applicable)
+                                            Complaince.NotApplicable +=1;
+                                    }
+                                ControlsDto.Add(Ctrol); 
+                            }
+                            DomainsDta.ChildrenControls = ControlsDto;
+                        }
+                    ListDomains.Add(DomainsDta);
+                    FrameworkDt.DomaindDta = ListDomains;
+                    Complaince.FrameworkData = FrameworkDt;
+                }
+                listComplaince.Add(Complaince);
+            };
+
+            return listComplaince;
         }
     }
 }
