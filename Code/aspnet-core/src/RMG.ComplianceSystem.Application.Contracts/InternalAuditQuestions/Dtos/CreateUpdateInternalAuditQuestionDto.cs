a57d447e2aff9142e3c06e59ffde9aa388c6a4b3
Amr Nabih Salah <73536162+amr-nabih@users.noreply.github.com>
2022-10-18 18:24:43 +0200

    Add InternalAuditQuestionAppService and  InternalAuditQuestion Dto and filter

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/InternalAuditQuestions/Dtos/CreateUpdateInternalAuditQuestionDto.cs
@@ -0,0 +1,16 @@
+﻿using RMG.ComplianceSystem.Attachments.Dtos;
+using RMG.ComplianceSystem.Risks.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMG.ComplianceSystem.InternalAuditQuestions
+{
+    [Serializable]
+    public class CreateUpdateInternalAuditQuestionDto
+    {
+        public string QuestionTextEn { get; set; }
+        public string QuestionTextAr { get; set; }
+        public Guid FrameworkId { get; set; }
+    }
+}
---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/InternalAuditQuestions/IInternalAuditQuestionAppService.cs
@@ -0,0 +1,20 @@
+﻿using RMG.ComplianceSystem.InternalAuditQuestions.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace RMG.ComplianceSystem.InternalAuditQuestions
+{
+    public interface IInternalAuditQuestionAppService :
+     ICrudAppService< //Defines CRUD methods
+         InternalAuditQuestionDto, //Used to show books
+         Guid, //Primary key of the book entity
+         InternalAuditQuestionPagedAndSortedResultRequestDto, //Used for paging/sorting
+         CreateUpdateInternalAuditQuestionDto> //Used to create/update a book
+    {
+        Task<PagedResultDto<InternalAuditQuestionDto>> GetListQuestionByFilterAsync(InternalAuditQuestionPagedAndSortedResultRequestDto input);
+    }
+}
