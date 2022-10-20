4414550684ac344071570ec1e5e117ca897a937e
DELL <DELL@DESKTOP-26I8A9T>
2022-10-20 22:01:07 +0200

    Create model and Dto and Service to proxy InternalAuditQuestion

---
+++ Code/angular/src/app/proxy/InternalAuditQuestions/dtos/models.ts
@@ -0,0 +1,23 @@
+import type { FullAuditedEntityWithUserDto, PagedAndSortedResultRequestDto } from '@abp/ng.core';
+import { IdentityUserDto } from '@abp/ng.account';
+
+export interface CreateUpdateInternalAuditQuestionDto {
+  FrameworkId?: string;
+  QuestionTextEn?: string;
+  QuestionTextAr?: string;
+  QuestionExplainEn?:string;
+  QuestionExplainAr?:string;
+}
+
+
+export interface InternalAuditQuestionDto extends FullAuditedEntityWithUserDto<IdentityUserDto, string> {
+  FrameworkId?: string;
+  QuestionTextEn?: string;
+  QuestionTextAr?: string;
+  QuestionExplainEn?:string;
+  QuestionExplainAr?:string;
+}
+
+export interface InternalAuditQuestionPagedAndSortedResultRequestDto extends PagedAndSortedResultRequestDto {
+  Search?: string;
+}
