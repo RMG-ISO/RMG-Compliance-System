50d3269678c567f1a36c013526326ed9f6a15821
Amr Nabih <a.nabih@rmg-sa.com>
2022-11-07 23:20:25 +0300

    create  Proxy Internal Audit preparation and service and Dto

---
+++ Code/angular/src/app/proxy/InternalAuditPreparations/InternalAuditPreparation.service.ts
@@ -0,0 +1,65 @@
+import type { CreateUpdateInternalAuditPreparationDto, InternalAuditPreparationDto, InternalAuditPreparationPagedAndSortedResultRequestDto} from './dtos/models';
+import { RestService } from '@abp/ng.core';
+import type {PagedResultDto } from '@abp/ng.core';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InternalAuditPreparationService {
+  apiName = 'Default';
+
+  create = (input: CreateUpdateInternalAuditPreparationDto) =>
+    this.restService.request<any, InternalAuditPreparationDto>({
+      method: 'POST',
+      url: '/api/app/internal-audit-preparation',
+      body: input,
+    },
+    { apiName: this.apiName });
+
+  delete = (id: string) =>
+    this.restService.request<any, void>({
+      method: 'DELETE',
+      url: `/api/app/internal-audit-preparation/${id}`,
+    },
+    { apiName: this.apiName });
+
+  get = (id: string) =>
+    this.restService.request<any, InternalAuditPreparationDto>({
+      method: 'GET',
+      url: `/api/app/internal-audit-preparation/menu-question-by-id/${id}`,
+    },
+    { apiName: this.apiName });
+
+
+
+  update = (id: string, input: CreateUpdateInternalAuditPreparationDto) =>
+    this.restService.request<any, InternalAuditPreparationDto>({
+      method: 'PUT',
+      url: `/api/app/internal-audit-preparation/${id}`,
+      body: input,
+    },
+    { apiName: this.apiName });
+
+
+    getList = (input: InternalAuditPreparationPagedAndSortedResultRequestDto) =>
+    this.restService.request<any, PagedResultDto<InternalAuditPreparationDto>>({
+      method: 'GET',
+      url: '/api/app/internal-audit-preparation/internal-audit-by-filter',
+      params: {Search:input.Search,DepartmentId:input.DepartmentId,FrameworkId:input.FrameworkId,sorting: input.sorting, skipCount: input.skipCount, maxResultCount: input.maxResultCount },
+    },
+    { apiName: this.apiName });
+
+    getByID = (id:string ) =>
+    this.restService.request<any, PagedResultDto<InternalAuditPreparationDto>>({
+      method: 'GET',
+      url: '/api/app/internal-audit-preparation/internal-audit-by-id',
+      params: { Id:id},
+    },
+    { apiName: this.apiName });
+
+
+
+
+  constructor(private restService: RestService) {}
+}
---
+++ Code/angular/src/app/proxy/InternalAuditPreparations/dtos/models.ts
@@ -0,0 +1,58 @@
+import type { FullAuditedEntityWithUserDto, PagedAndSortedResultRequestDto } from '@abp/ng.core';
+import type { InternalAuditQuestionDto } from '@proxy/InternalAuditQuestions/dtos/models';
+
+import { IdentityUserDto } from '@abp/ng.account';
+
+export interface CreateUpdateInternalAuditPreparationDto {
+  AuditCode?: string;
+  AuditTitleEn?: string;
+  AuditTitleAr?: string;
+  AuditDescriptionEn?:string;
+  AuditDescriptionAr?:string;
+  AuditFieldEn?:string;
+  AuditFieldAr?:string;
+  AuditSetpsEn?:string;
+  AuditSetpsAr?:string;
+  StartDate?:Date;
+  EndDate?:Date;
+  RiskOpportunityId?:string;
+  FrameworkId?:string;
+  Auditors?:InternalAuditorDto[];
+}
+
+export interface InternalAuditorDto{
+  InternalAuditPreparationId?: string;
+  UserId?: string;
+  DepartmentId?: string;
+  IsAuditor?:boolean;
+}
+
+export interface AuditorDto{
+  InternalAuditPreparationId?: string;
+  UserId?: string;
+  IsAuditor?:boolean;
+}
+
+export interface InternalAuditPreparationDto extends FullAuditedEntityWithUserDto<IdentityUserDto, string> {
+  AuditCode?: string;
+  AuditTitleEn?: string;
+  AuditTitleAr?: string;
+  AuditDescriptionEn?:string;
+  AuditDescriptionAr?:string;
+  AuditFieldEn?:string;
+  AuditFieldAr?:string;
+  AuditSetpsEn?:string;
+  AuditSetpsAr?:string;
+  StartDate?:Date;
+  EndDate?:Date;
+  RiskOpportunityId?:string;
+  FrameworkId?:string;
+  AuditorDeptDto?:InternalAuditorDto[];
+  AuditorDto?:AuditorDto[];
+}
+
+export interface InternalAuditPreparationPagedAndSortedResultRequestDto extends PagedAndSortedResultRequestDto {
+  Search?: string;
+  FrameworkId?: string;
+  DepartmentId ?: string;
+}
