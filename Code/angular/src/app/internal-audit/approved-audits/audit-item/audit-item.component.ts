f1b16a94c89f0d6d6b48e998086ff76a4fec8273
Mustafa Mohamed <mustafa2018mohamed@gmail.com>
2022-11-10 20:04:30 +0200

    update

--- Code/angular/src/app/internal-audit/approved-audits/audit-item/audit-item.component.ts
+++ Code/angular/src/app/internal-audit/approved-audits/audit-item/audit-item.component.ts
@@ -1,6 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { ConfigStateService } from '@abp/ng.core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { InternalAuditPreparationService } from '@proxy/InternalAuditPreparations';
 
 @Component({
   selector: 'app-audit-item',
@@ -9,24 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuditItemComponent implements OnInit {
   form:FormGroup;
-
+  isSaving = false;
+  auditInfo;
   constructor(
-    private configStateService:ConfigStateService
+    private configStateService:ConfigStateService,
+    private activatedRoute:ActivatedRoute,
+    private internalAuditPreparationService:InternalAuditPreparationService,
+
   ) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       isApprove:new FormControl(true),
-      approveDate:new FormControl(),
+      // approveDate:new FormControl(),
       approveBy:new FormControl(this.configStateService.getAll().currentUser.id),
-      causesRefuse:new FormControl(),
+      causesRefuse:new FormControl({value:null, disabled:true}),
     });
     
+    this.internalAuditPreparationService.getByID(this.activatedRoute.snapshot.params.id).subscribe(r => this.auditInfo = r)
   }
 
   changeSelection(ev) {
     console.log(ev);
     if(ev.value) this.form.controls.causesRefuse.setValue(null);
+
+    ev.value ? this.form.controls.causesRefuse.disable() : this.form.controls.causesRefuse.enable();
 
     this.form.controls.causesRefuse.setValidators(ev.value ? null : Validators.required);
     this.form.controls.causesRefuse.updateValueAndValidity();
