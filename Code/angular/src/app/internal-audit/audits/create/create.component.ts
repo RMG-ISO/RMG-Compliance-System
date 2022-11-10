ac6c9f8c485ef91dd8e3f00a48c4782b74e99fd1
Mustafa Mohamed <mustafa2018mohamed@gmail.com>
2022-11-10 07:49:30 +0200

    ff

--- Code/angular/src/app/internal-audit/audits/create/create.component.ts
+++ Code/angular/src/app/internal-audit/audits/create/create.component.ts
@@ -1,6 +1,10 @@
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { FrameworkService } from '@proxy/frameworks';
+import { InternalAuditPreparationService } from '@proxy/InternalAuditPreparations';
+import { DepartmentService } from '@proxy/departments';
+import { EmployeeService } from '@proxy/employees';
 
 @Component({
   selector: 'app-create',
@@ -10,9 +14,16 @@ import { Component, OnInit } from '@angular/core';
 export class CreateComponent implements OnInit {
   form:FormGroup;
   mode;
+  frameworks;
+  departments;
+  allEmployees;
 
   constructor(
-    private activatedRoute:ActivatedRoute
+    private activatedRoute:ActivatedRoute,
+    private frameworkService:FrameworkService,
+    private internalAuditPreparationService:InternalAuditPreparationService,
+    private departmentService: DepartmentService,
+    private employeeService: EmployeeService,
   ) { }
 
   ngOnInit(): void {
@@ -27,14 +38,37 @@ export class CreateComponent implements OnInit {
       auditFieldAr: new FormControl(null, Validators.required),
       auditSetpsEn: new FormControl(null, Validators.required),
       auditSetpsAr: new FormControl(null, Validators.required),
+      auditGoalsEn: new FormControl(null, Validators.required),
+      auditGoalsAr: new FormControl(null, Validators.required),
+      frameworkId: new FormControl(null, Validators.required),
       departmentId: new FormControl(null, Validators.required),
       startDate: new FormControl(null, Validators.required),
       endDate: new FormControl(null, Validators.required),
       riskOpportunityId: new FormControl(null, Validators.required),
-      frameworkId: new FormControl(null, Validators.required),
       auditors:new FormControl(null, Validators.required),
       departmentRepresentatives:new FormControl(null, Validators.required),
     });
+
+    
+
+  }
+
+  getItems() {
+    this.frameworkService.getList({maxResultCount:null}).subscribe(result => {
+      this.frameworks = result.items;
+    });
+
+    this.departmentService.getList({maxResultCount:null}).subscribe(result => {
+      this.departments = result.items;
+    });
+ 
+    this.employeeService.getList({maxResultCount:null}).subscribe(result => {
+      this.allEmployees = result.items;
+    });
+  }
+
+  getEmployeesByDepartment() {
+    // this.employeeService.getList()
   }
 
   save() {
