3c84cd3e636b0193a10acd0cf911a6864de5871c
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-03-06 13:29:51 +0200

    add frameworks page

--- Code/angular/src/app/frameworks/frameworks-list/frameworks-list.component.ts
+++ Code/angular/src/app/frameworks/frameworks-list/frameworks-list.component.ts
@@ -1,6 +1,6 @@
 import { ListService, LocalizationService, SessionStateService } from '@abp/ng.core';
 import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
@@ -16,6 +16,7 @@ import { FormMode } from 'src/app/shared/interfaces/form-mode';
   providers:[ListService]
 })
 export class FrameworksListComponent implements OnInit {
+  @ViewChild('formDirective') formDirective;
 
   FormMode = FormMode;
   sharedStatusOptions = sharedStatusOptions;
@@ -56,7 +57,10 @@ export class FrameworksListComponent implements OnInit {
   delete(model: FrameworkDto) {
     this.confirmation.warn('::FrameworkDeletionConfirmationMessage', '::AreYouSure', { messageLocalizationParams: [this.localizationService.currentLang.includes('ar') ?  model.nameAr : model.nameEn] }).subscribe((status) => {
       if (status === Confirmation.Status.confirm) {
-        this.frameworkService.delete(model.id).subscribe(() => this.list.get());
+        this.frameworkService.delete(model.id).subscribe(() => {
+          if(model.id == this.selected?.id) this.setFormData(null);
+          this.list.get()
+        });
       }
     });
   }
@@ -73,20 +77,48 @@ export class FrameworksListComponent implements OnInit {
 
 
   buildForm() {
-    this.form = new FormGroup({
-      nameAr: new FormControl(null, Validators.required),
-      nameEn: new FormControl(null, Validators.required),
-      shortcutAr: new FormControl(null, Validators.required),
-      shortcutEn: new FormControl(null, Validators.required),
-      descriptionAr: new FormControl(null),
-      descriptionEn: new FormControl(null),
-      status: new FormControl(null, Validators.required),
-      id: new FormControl(null)
-    })
+    // this.form = null;
+    if(!this.form) this.form = new FormGroup({
+        nameAr: new FormControl(null, Validators.required),
+        nameEn: new FormControl(null, Validators.required),
+        shortcutAr: new FormControl(null, Validators.required),
+        shortcutEn: new FormControl(null, Validators.required),
+        descriptionAr: new FormControl(null),
+        descriptionEn: new FormControl(null),
+        status: new FormControl(null, Validators.required),
+        id: new FormControl(null)
+      });
+   
+
     this.form.patchValue(this.selected);
+    console.log(this.selected);
+    if(!this.selected) {
+      this.form.reset();
+
+      for(let c in this.form.controls) {
+        this.form.controls[c].setErrors(null);
+        this.form.controls[c].reset();
+        this.form.controls[c].markAsPristine();
+        this.form.controls[c].markAsUntouched();
+        this.form.controls[c].updateValueAndValidity();
+    }
+
+      // this.formDirective.resetForm();
+
+      // this.form.markAsPristine();
+      // this.form.markAsUntouched();
+      // this.form.updateValueAndValidity();
+
+    }
+
+    console.log(this.form);
+
+    console.log('/formDirective', this.formDirective);
+
   }
 
   save() {
+    console.log(this.form);
     if (this.form.invalid) return;
 
     const request = this.selected?.id
@@ -94,7 +126,7 @@ export class FrameworksListComponent implements OnInit {
       : this.frameworkService.create(this.form.value);
 
     request.subscribe(() => {
-      this.form.reset();
+      this.setFormData(null)
       this.list.get();
     });
   }
