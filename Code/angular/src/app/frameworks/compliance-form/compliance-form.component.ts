e1f5bc2eceb5dcb4cfde2be878ee0b57f0499b6a
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-05-30 18:18:09 +0300

    ss

--- Code/angular/src/app/frameworks/compliance-form/compliance-form.component.ts
+++ Code/angular/src/app/frameworks/compliance-form/compliance-form.component.ts
@@ -14,35 +14,61 @@ export class ComplianceFormComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       isApplicable : new FormControl(null),
-      level:new FormControl(null, Validators.required),
+      level:new FormControl(0),
       date:new FormControl(null, Validators.required),
       nextDate:new FormControl(null, Validators.required),
-      trusted:new FormControl(1, Validators.required),
+      trusted:new FormControl(null, Validators.required),
+      trustedValue:new FormControl(null, [Validators.min(1), Validators.max(99)]),
       completed:new FormControl(null, Validators.required),
+      completedValue:new FormControl(null, [Validators.min(1), Validators.max(99)]),
       effective:new FormControl(null, Validators.required),
+      effectiveValue:new FormControl(null, [Validators.min(1), Validators.max(99)]),
       comment:new FormControl(null),
       reviewerComment:new FormControl(null),
       attachmentId:new FormControl(null),
+      addFiles:new FormControl(null),
     })
   }
 
   changeApplicable(event) {
-    console.log(event)
+    console.log(event);
     event.value
   }
 
+
+  partsControls = ['trusted','completed','effective']
+  changeAnswer(value, control) {
+    this.form.controls[control + 'Value'].setValidators(value === 2 ? [Validators.required, Validators.min(1), Validators.max(99)] : null);
+    this.form.controls[control + 'Value'].updateValueAndValidity();
+
+    let mustAddFiles = null;
+    for(let c of this.partsControls) {
+      let value = this.form.controls[c].value;
+      if(value == 1 || value == 2) {
+        mustAddFiles = [Validators.required];
+        break;
+      }
+    }
+
+    this.form.controls.comment.setValidators(mustAddFiles);
+    this.form.controls.addFiles.setValidators(mustAddFiles);
+    
+    this.form.controls.comment.updateValueAndValidity();
+    this.form.controls.addFiles.updateValueAndValidity();
+  }
 
   OnFileUploaded(attachmentId: string) {
     this.form.controls["attachmentId"].patchValue(attachmentId);
   }
 
-  uploading
+  uploading;
   OnFileBeginUpload(beginUpload: boolean) {
     this.uploading = true;
   }
 
   OnFileEndUpload(endUpload: boolean) {
     this.uploading = false;
+    this.form.controls.addFiles.setValue(true);
   }
 
   save() {
