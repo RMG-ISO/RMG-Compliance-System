22b85571ce08c97d70b1f79fb3bcb649fa330a86
wael-razouk <wael.razouk.1991@gmail.com>
2023-06-08 07:44:13 +0300

    implement real values for some dashboard numbers

--- Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Frameworks/Dtos/FrameworkDto.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Frameworks/Dtos/FrameworkDto.cs
@@ -31,6 +31,7 @@ namespace RMG.ComplianceSystem.Frameworks.Dtos
         public Guid? AttachmentId { get; set; }
         public Guid ReviewUserId { get; set; }
         public Guid ApproveUserId { get; set; }
+        public string approveUserName { get; set; }
         public string LevelFirstNameAr { get; set; }
         public string LevelFirstNameEn { get; set; }
         public string LevelSecondNameAr { get; set; }
@@ -40,7 +41,7 @@ namespace RMG.ComplianceSystem.Frameworks.Dtos
         public Guid OwnerId { get; set; }
         public string LevelFourNameAr { get; set; }
         public string LevelFourNameEn { get; set; }
-
+        public bool HasMainControl { get; set; }
         public ComplianceStatus ComplianceStatus { get; set; }
         public DateTime? SelfAssessmentStartDate { get; set; }
         public DateTime? SelfAssessmentEndDate { get; set; }
