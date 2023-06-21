b0845b816a450f8217d28fcf56e42c5c359686e7
wael-razouk <wael.razouk.1991@gmail.com>
2023-06-21 14:14:34 +0300

    revert IdentityUserDto model in front

--- Code/angular/src/app/proxy/frameworks/dtos/models.ts
+++ Code/angular/src/app/proxy/frameworks/dtos/models.ts
@@ -1,4 +1,5 @@
 import type { FullAuditedEntityWithUserDto, PagedAndSortedResultRequestDto } from '@abp/ng.core';
+import { IdentityUserDto } from '@abp/ng.account';
 import type { SharedStatus } from '../../shared/shared-status.enum';
 import type { FrameworkStatus } from '../../shared/framework-status.enum';
 import type { ComplianceStatus } from '../../shared/compliance-status.enum';
@@ -7,7 +8,7 @@ import type { ControlDto } from '../../controls/dtos/models';
 import type { AssessmentDto } from '../../assessments/dtos/models';
 import type { DomainDto } from '../../domains/dtos/models';
 
-export interface FrameworkDto extends FullAuditedEntityWithUserDto<string, IdentityUserDto> {
+export interface FrameworkDto extends FullAuditedEntityWithUserDto<IdentityUserDto, string> {
   managementName?: string;
   reviewUserName?: string;
   ownerName?: string;
@@ -91,7 +92,7 @@ export interface FrameworkData {
   domainDta: MainDomainsDto[];
 }
 
-export interface FrameworkEmployeeDto extends FullAuditedEntityWithUserDto<string, IdentityUserDto> {
+export interface FrameworkEmployeeDto extends FullAuditedEntityWithUserDto<IdentityUserDto, string> {
   frameworkId?: string;
   employeeId?: string;
 }
