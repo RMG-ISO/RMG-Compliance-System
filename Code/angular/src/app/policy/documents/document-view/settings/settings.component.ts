37944bf6d5527bf4f316e8cd5d5a13b58d5d327a
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-07-23 21:32:54 +0300

    fixes

--- Code/angular/src/app/policy/documents/document-view/settings/settings.component.ts
+++ Code/angular/src/app/policy/documents/document-view/settings/settings.component.ts
@@ -4,6 +4,9 @@ import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import { MatDialog } from '@angular/material/dialog';
 import { FormMode } from 'src/app/shared/interfaces/form-mode';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DocumentSectionService } from '@proxy/Documents/document-section.service';
+import { finalize } from 'rxjs';
+import { ToasterService } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-settings',
@@ -13,14 +16,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SettingsComponent implements OnInit {
 
   constructor(
-    private matDialog:MatDialog
-  ) {
-    
-  }
+    private matDialog:MatDialog,
+    private documentSectionService:DocumentSectionService,
+    private toasterService:ToasterService
+  ) { }
   
   documentData
   ngOnInit() {
-    console.log(this.documentData)
+    console.log(this.documentData);
+    this.getSections();
   }
 
 
@@ -41,6 +45,12 @@ export class SettingsComponent implements OnInit {
     };
   }
   
+  getSections() {
+    this.documentSectionService.getList({documentId: this.documentData.id, maxResultCount:null}).subscribe( r => {
+      console.log(r);
+
+    })
+  }
 
   openSectionDialog(section, data?) {
     this.buildForm(data);
@@ -50,22 +60,34 @@ export class SettingsComponent implements OnInit {
         mode:data ? FormMode.Edit : FormMode.Create
       },
     });
-
-    ref.afterClosed().subscribe(con => {
-      
-    });
   }
 
   sectionForm:FormGroup;
   buildForm(data = {}) {
     this.sectionForm = new FormGroup({
       title: new FormControl(null, Validators.required),
-      content: new FormControl(null),
+      content: new FormControl('content'),
       documentId: new FormControl(this.documentData.id, Validators.required),
+      id: new FormControl(null)
     });
 
     this.sectionForm.patchValue(data);
   }
 
+
+  isSaving = false;
+  save(ref) {
+    if(this.sectionForm.invalid) return;
+    this.isSaving = true;
+
+    let value = this.sectionForm.value;
+    (value.id ? this.documentSectionService.update(value.id, value) : this.documentSectionService.create(value))
+    .pipe(finalize(() => this.isSaving = false))
+    .subscribe(r => {
+      this.toasterService.success( value.id ? '::UpdatedSuccessfully' : '::CreatedSuccessfully' , "");
+      ref.close(true);
+      this.getSections();
+    })
+  }
 
 }
