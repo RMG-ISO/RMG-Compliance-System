a883d80731f0e56b6903d87bb39a6899c27a2b08
wael-razouk <wael.razouk.1991@gmail.com>
2023-07-23 15:53:01 +0300

    document sections CRUD backend

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Documents/Dtos/DocumentSectionDto.cs
@@ -0,0 +1,17 @@
+﻿using RMG.ComplianceSystem.Policies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace RMG.ComplianceSystem.Documents.Dtos
+{
+    public class DocumentSectionDto : FullAuditedEntityDto<Guid>
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Order { get; set; }
+        public DocumentSectionStatus Status { get; set; }
+        public Guid DocumentId { get; set; }
+    }
+}
---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Domain.Shared/Policies/DocumentSectionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMG.ComplianceSystem.Policies
+{
+    public enum DocumentSectionStatus
+    {
+        Draft = 1,
+        Completed
+    }
+}
