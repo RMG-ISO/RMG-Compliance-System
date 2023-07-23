e29f670ab936883ada4ef31b9329677be4dc5464
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-07-24 01:21:43 +0300

    ff

--- Code/angular/src/app/policy/documents/document-view/settings/settings.component.ts
+++ Code/angular/src/app/policy/documents/document-view/settings/settings.component.ts
@@ -26,14 +26,16 @@ export class SettingsComponent implements OnInit {
   documentData
   
   form:FormGroup;
-
+  sectionsFormArr:FormArray;
   ngOnInit() {
     console.log(this.documentData);
     this.getSections();
 
     this.form = new FormGroup({
       sections: new FormArray([])
-    })
+    });
+
+    this.sectionsFormArr = (this.form.controls.sections as FormArray);
   }
 
   addSection(data) {
@@ -42,10 +44,13 @@ export class SettingsComponent implements OnInit {
       content: new FormControl(null),
       id: new FormControl(null),
       order: new FormControl(null),
+      documentId: new FormControl(null),
     });
-    group.patchValue(data);
+    group.patchValue(data || {documentId: this.documentData.id});
 
-    (this.form.controls.sections as FormArray).push(group);
+    this.sectionsFormArr.push(group);
+
+
   }
 
 
@@ -59,28 +64,40 @@ export class SettingsComponent implements OnInit {
 
   public onReady(editor) {
     editor.ui
-      .getEditableElement()
-      .parentElement.insertBefore(editor.ui.view.toolbar.element, editor.ui.getEditableElement());
+    .getEditableElement()
+    .parentElement.insertBefore(editor.ui.view.toolbar.element, editor.ui.getEditableElement());
     editor.plugins.get('FileRepository').createUploadAdapter = function (loader) {
       return new UploadAdapter(loader);
     };
   }
   
-  sections;
+  sections = [];;
   getSections() {
     this.documentSectionService.getList({documentId: this.documentData.id, maxResultCount:null}).subscribe( r => {
-      this.sections = r.items;
-      let sections = (this.form.controls.sections as FormArray);
-      sections.reset();
+      this.sections = r.items.sort( (a,b) => {
+        if(a.order > b.order) return 1;
+        if(a.order < b.order) return -1;
+        return 0;
+      });
+      // let sections = this.sectionsFormArr;
+      this.sectionsFormArr.reset();
       for(let section of this.sections) this.addSection(section);
       console.log(this.sections);
     })
   }
 
   drop(event: CdkDragDrop<{title: string; poster: string}[]>) {
-    moveItemInArray((this.form.controls.sections as FormArray).controls , event.previousIndex, event.currentIndex);
+    moveItemInArray(this.sectionsFormArr.controls , event.previousIndex, event.currentIndex);
+    
+    this.updateOrders();
   }
-  
+
+  updateOrders() {
+    this.documentSectionService.changeOrdersByIdAndSections(this.documentData.id, this.sectionsFormArr.getRawValue().map(g => g.id)).subscribe();
+    this.sections = this.sectionsFormArr.getRawValue();
+  }
+
+
   activeIndex;
   openSectionDialog(section, data?, index = null) {
     this.activeIndex = index;
@@ -91,7 +108,6 @@ export class SettingsComponent implements OnInit {
         mode:data ? FormMode.Edit : FormMode.Create
       },
     });
-
   }
 
   sectionForm:FormGroup;
@@ -107,21 +123,34 @@ export class SettingsComponent implements OnInit {
   }
 
   delete(index) {
-    // this.documentSectionService.delete()
-    // (this.form.controls.sections as FormArray).removeAt(i);
-    let arr = (this.form.controls.sections as FormArray);
+    // let arr = (this.form.controls.sections as FormArray);
 
-    let model = arr.controls[index].value
+    let model = this.sectionsFormArr.controls[index].value
     this.confirmation.warn('::FrameworkDeletionConfirmationMessage', '::AreYouSure', { messageLocalizationParams: [ model.title ] }).subscribe((status) => {
       if (status === Confirmation.Status.confirm) {
         this.documentSectionService.delete(model.id).subscribe( () => {
           (this.form.controls.sections as FormArray).removeAt(index);
+          this.sections = this.sectionsFormArr.getRawValue();
         });
       }
     });
-
   }
 
+  updatedIndex = null;
+  updateContent() {
+    console.log('updatedIndex', this.updatedIndex);
+    let updateGroup = this.sectionsFormArr.controls[this.updatedIndex];
+    if(updateGroup.invalid) return;
+
+    this.documentSectionService.update(updateGroup.value.id, updateGroup.value).subscribe(r => {
+      this.sections[this.updatedIndex] = r;
+    });
+  }
+
+  resetSection(index) {
+    let group = this.sectionsFormArr.controls[index] as FormGroup;
+    group.controls.content.setValue(this.sections[index].content);
+  }
 
   isSaving = false;
   save(ref) {
@@ -139,6 +168,8 @@ export class SettingsComponent implements OnInit {
       else {
         (this.form.controls.sections as FormArray).controls[this.activeIndex].patchValue(r);
       }
+
+      this.updateOrders();
     })
   }
 
