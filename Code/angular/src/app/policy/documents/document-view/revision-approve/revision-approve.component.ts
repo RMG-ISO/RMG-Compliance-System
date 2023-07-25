de8cb2d174299e3827a99d61627e81ac1ce2b0d0
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-07-25 15:56:58 +0300

    fixes

--- Code/angular/src/app/policy/documents/document-view/revision-approve/revision-approve.component.ts
+++ Code/angular/src/app/policy/documents/document-view/revision-approve/revision-approve.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { ListService, LocalizationService } from '@abp/ng.core';
+import { ConfirmationService } from '@abp/ng.theme.shared';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-revision-approve',
   templateUrl: './revision-approve.component.html',
-  styleUrls: ['./revision-approve.component.scss']
+  styleUrls: ['./revision-approve.component.scss'],
+  providers:[ListService]
 })
-export class RevisionApproveComponent {
+export class RevisionApproveComponent implements OnInit {
 
+  constructor(
+    public readonly list: ListService,
+    public  matDialog: MatDialog,
+    private confirmation: ConfirmationService,
+    private router: Router,
+    private localizationService: LocalizationService,
+    private activatedRoute:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.getList();
+  }
+
+  items;
+  totalCount;
+  getList() {
+    const streamCreator = (query) => this.listS;
+    this.list.hookToQuery(streamCreator).subscribe((response) => {
+      this.items = response.items;
+      this.totalCount = response.totalCount;
+    });
+  }
+
+  listS = new BehaviorSubject({
+    items:[
+      {
+        name:'name 1',
+        role: 'مراجع',
+        action:'موافقة',
+        date:'2023/7/25',
+        status:1,
+        notes:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat'
+      },
+      {
+        name:'name 2',
+        role: 'مراجع',
+        action:'موافقة',
+        date:'2023/7/25',
+        status:2,
+        notes:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita'
+      },
+      {
+        name:'name 3',
+        role: 'مراجع',
+        action:'موافقة',
+        date:'2023/7/25',
+        status:1,
+        notes:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita'
+      },
+      {
+        name:'name 4',
+        role: 'مراجع',
+        action:'موافقة',
+        date:'2023/7/25',
+        status:1,
+        notes:'Lorem ipsum dolor sit amet, consetetur. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita'
+      },
+      {
+        name:'name 5',
+        role: 'مراجع',
+        action:'موافقة',
+        date:'2023/7/25',
+        status:1,
+        notes:'Lorem ipsum dolor sit amet dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita'
+      },
+    ],
+    totalCount:5
+  })
 }
