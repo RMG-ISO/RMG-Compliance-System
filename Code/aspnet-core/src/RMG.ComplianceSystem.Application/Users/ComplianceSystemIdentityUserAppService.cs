19ff5ab01296d8cc0d7ed5f73847b57ee43982ee
Saeed Alrabid <s.alrabid@rmg-sa.com>
2023-08-02 11:08:41 +0300

    override create user app service

---
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application/Users/ComplianceSystemIdentityUserAppService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
+using static RMG.ComplianceSystem.Subscription.SubscriptionDate;
+
+namespace RMG.ComplianceSystem.IdentityUser
+{
+    [Dependency(ReplaceServices = true)]
+    [ExposeServices(typeof(IIdentityUserAppService), typeof(IdentityUserAppService), typeof(ComplianceSystemIdentityUserAppService))]
+    public class ComplianceSystemIdentityUserAppService : IdentityUserAppService
+    {
+        public ComplianceSystemIdentityUserAppService(IdentityUserManager userManager, IIdentityUserRepository userRepository, IIdentityRoleRepository roleRepository, IOptions<IdentityOptions> identityOptions) : base(userManager, userRepository, roleRepository, identityOptions)
+        {
+        }
+
+
+        public override async Task<IdentityUserDto> CreateAsync(IdentityUserCreateDto input)
+        {
+            var usersCount = await UserRepository.GetCountAsync();
+            if (usersCount >= MaxUserCount)
+                throw new UserFriendlyException(L["CreateUserError"]);
+            return await base.CreateAsync(input);
+        }
+    }
+}
