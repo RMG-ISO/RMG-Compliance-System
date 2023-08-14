fd33624de88314de7074b8fc18d0badda91f98e2
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-08-14 11:39:06 +0300

    fix ui

--- Code/angular/src/app/shared/components/error-intercept/error-intercept.component.ts
+++ Code/angular/src/app/shared/components/error-intercept/error-intercept.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-intercept',
@@ -8,17 +9,26 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ErrorInterceptComponent implements OnInit {
   errorStatus;
-  title;
+  title:string;
   content;
   showIcon = true;
   iconName = 'warning';
   iconColor = 'warn'
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: ErrorInterceptComponent) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ErrorInterceptComponent, private router:Router) {
     Object.assign(this,data || {})
   }
+
+  returnUrl;
   ngOnInit(): void {
     console.log(this);
+    if(this.title.includes('403') || this.title.includes('401')) {
+      this.returnUrl = '/home'
+    }
+  }
+
+  route() {
+    if(this.returnUrl) this.router.navigate([this.returnUrl])
   }
 
 }
