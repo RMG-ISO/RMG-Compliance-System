885567f445a675c0acad63dcaa9e980c654ce5d8
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-08-14 11:32:17 +0300

    add inter

--- Code/angular/src/app/shared/components/error-intercept/error-intercept.component.ts
+++ Code/angular/src/app/shared/components/error-intercept/error-intercept.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-error-intercept',
   templateUrl: './error-intercept.component.html',
   styleUrls: ['./error-intercept.component.scss']
 })
-export class ErrorInterceptComponent  {
+export class ErrorInterceptComponent implements OnInit {
   errorStatus;
+  title;
+  content;
+  showIcon = true;
+  iconName = 'warning';
+  iconColor = 'warn'
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ErrorInterceptComponent) {
+    Object.assign(this,data || {})
+  }
+  ngOnInit(): void {
+    console.log(this);
+  }
+
 }
