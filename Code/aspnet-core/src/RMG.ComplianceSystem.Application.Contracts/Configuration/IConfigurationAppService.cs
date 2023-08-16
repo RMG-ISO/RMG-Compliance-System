6cf000c91d89b240e251ba7e01ceea03c93e3802
wael-razouk <wael.razouk.1991@gmail.com>
2023-08-16 15:50:00 +0300

    fix saving app logo

--- Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Configuration/IConfigurationAppService.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application.Contracts/Configuration/IConfigurationAppService.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
+using Volo.Abp.Content;
 
 namespace RMG.ComplianceSystem.Configuration
 {
     public interface IConfigurationAppService : IApplicationService
     {
         Task<List<ConfigurationDto>> GetAll();
-        Task UpdateLogo(string logo);
+        Task UpdateLogo(UpdateLogoDto dto);
     }
 }
--- Code/aspnet-core/src/RMG.ComplianceSystem.Application/Configuration/ConfigurationAppService.cs
+++ Code/aspnet-core/src/RMG.ComplianceSystem.Application/Configuration/ConfigurationAppService.cs
@@ -1,9 +1,15 @@
-﻿using RMG.ComplianceSystem.Configuration.Dtos;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using RMG.ComplianceSystem.Attachments;
+using RMG.ComplianceSystem.Configuration.Dtos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.BlobStoring;
+using Volo.Abp.Content;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.Settings;
 
@@ -12,21 +18,29 @@ namespace RMG.ComplianceSystem.Configuration
     public class ConfigurationAppService : ComplianceSystemAppService, IConfigurationAppService
     {
         private readonly ISettingManager _settingManager;
+        private readonly IBlobContainer<AttachmentFileContainer> _fileContainer;
 
-        public ConfigurationAppService(ISettingManager settingManager)
+        public ConfigurationAppService(ISettingManager settingManager, IBlobContainer<AttachmentFileContainer> fileContainer)
         {
             _settingManager = settingManager;
+            _fileContainer = fileContainer;
         }
 
         public async Task<List<ConfigurationDto>> GetAll()
         {
             var list = await _settingManager.GetAllGlobalAsync();
-            return list.Where(s => s.Name.StartsWith("App.")).Select(s => new ConfigurationDto(s.Name, s.Value, L[s.Name])).ToList();
+            var res = list.Where(s => s.Name.StartsWith("App.")).Select(s => new ConfigurationDto(s.Name, s.Value, L[s.Name])).ToList();
+            var logo = res.FirstOrDefault(r => r.Name == "App.Logo");
+            if (logo != null)
+                logo.Value = Encoding.Default.GetString((await _fileContainer.GetAsync(logo.Value)).GetAllBytes());
+            return res;
         }
 
-        public async Task UpdateLogo(string logo)
+        public async Task UpdateLogo(UpdateLogoDto dto)
         {
-            await _settingManager.SetGlobalAsync("App.Logo", logo);
+            var guid = GuidGenerator.Create().ToString();
+            await _fileContainer.SaveAsync(guid, dto.ImageAsBase64.GetBytes());
+            await _settingManager.SetGlobalAsync("App.Logo", guid);
         }
 
     }
